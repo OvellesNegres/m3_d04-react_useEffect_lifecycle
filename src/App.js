bd02e5adf4023309918488304339cc034aa7e4a5
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-12-02 15:02:28 +0100

    starter code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import { useState } from "react";
 import "./App.css";
-// import Counter from "./components/01-use-state/Counter";
-// import Timer from "./components/02-use-state-loop/Timer";
-// import TimerTwo from "./components/03-use-effect-mounting/TimerTwo";
-// import TimerThree from "./components/04-use-effect-unmounting/TimerThree";
-// import TimerFour from "./components/05-use-effect-conditional-updates/TimerFour";
 
 import IronbnbList from './components/IronbnbList';  // <== IMPORT
 
@@ -13,16 +8,6 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={() => setShow(!show)}>
-        { show ? "Hide" : "Show"}
-      </button>
-      {/* {show && <Counter />} */}
-      {/* {show && <Timer />} */}
-      {/* {show && <TimerTwo />} */}
-      {/* { show && <TimerThree /> } */}
-      {/* {show && <TimerFour />} */}
-      
-      {show && <IronbnbList />}                 {/*  <== ADD  */}
     </div>
   );
 }
---
+++ src/components/TimerFour.js
@@ -0,0 +1,9 @@
+// ./src/components/TimerFour.js
+import React, { useState, useEffect } from "react";
+
+function TimerFour() {
+
+  return(null)
+}
+
+export default TimerFour;
